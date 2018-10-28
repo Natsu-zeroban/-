d488b78d6ebd1e27c9912bc9a2ef56f9cb3ce881
scultc <38917890+scultc@users.noreply.github.com>
2018-10-28 17:19:06 +0800

    界面

---
+++ 二面/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $("#submit").click(function () {
+        $("#result").slideDown(500);
+    });
+    $("#return").click(function () {
+        $("#result").slideUp(500);
+    })
+});
